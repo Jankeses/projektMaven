c152ace0f8ec016567ae013527fd6ddf35ffbdd8
Janek <jan.drychta@gmail.com>
2019-10-18 09:42:32 +0200

    Poprawa logiki

--- src/main/java/LiczbyPierwsze.java
+++ src/main/java/LiczbyPierwsze.java
@@ -1,12 +1,11 @@
 public class LiczbyPierwsze {
     public static boolean czyJestPierwsza (int a) {
-        for (int i =2; i <=a; i++) {
-            if (a%i != 0){
-            break;
+        for (int i = 2; i <a/2 ; i++) {
+            if (a%i == 0){
+                return false;
             }
-            return true;
         }
-        return false;
+        return true;
     }
     // Math.round(Math.sqrt(a)
 }
